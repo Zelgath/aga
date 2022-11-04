cbeb3012dc3b94e043af3d6d76435c9dfba52c8f
Sebastian Mataczynski <sebastian.mataczynski.external@atos.net>
2022-11-04 14:20:39 +0100

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FlexLayoutModule } from '@angular/flex';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProfilePicComponent } from './profile-pic/profile-pic.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    FooterComponent,
+    ProfilePicComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    FontAwesomeModule,
+    FontAwesomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
-  constructor() { }
-
+  faBars = faBars;
 }
